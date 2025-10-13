a55a33de895e52ac363b07bc08de42b2af1ec86c
unknown <juniorlevenhagen@gmail.com>
2025-10-13 16:20:08 -0300

    pagina principal resolvida.

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -10,6 +10,10 @@ export function Navbar() {
     router.push("/auth/login");
   };
 
+  const handleStartNow = () => {
+    router.push("/register/step0");
+  };
+
   return (
     <nav className="w-full bg-white px-4 sm:px-6 py-3 sm:py-4 flex justify-between items-center">
       {/* Logo à esquerda */}
@@ -37,11 +41,21 @@ export function Navbar() {
         <button className="text-sm px-3 mr-3 sm:text-base text-black font-zalando font-medium hover:text-gray-700 transition-colors duration-200">
           Blog
         </button>
+
+        {/* Botão "Entrar" */}
         <button
           onClick={handleEnter}
-          className="sm:px-6 py-2 text-xs sm:text-base text-white font-zalando font-medium bg-[#4AA4F7] hover:bg-gray-800 transition-all duration-200 rounded-lg"
+          className="sm:px-6 py-2 text-xs sm:text-base text-white font-zalando font-medium bg-[#4AA4F7] hover:bg-[#F59C46] transition-all duration-200 rounded-lg mr-2"
         >
           Entrar
+        </button>
+
+        {/* Botão "Comece agora" */}
+        <button
+          onClick={handleStartNow}
+          className="sm:px-6 py-2 text-xs sm:text-base text-white font-zalando font-medium bg-black hover:bg-gray-600 transition-all duration-200 rounded-lg"
+        >
+          Comece agora
         </button>
       </div>
     </nav>

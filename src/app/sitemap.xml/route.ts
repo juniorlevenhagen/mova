b22573a909dd31e10d368729b73f0cf3f3d238c3
unknown <juniorlevenhagen@gmail.com>
2026-02-23 12:31:40 -0300

    feat: sitemap actualization

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://movamais.fit</loc>
+    </url>
+  </urlset>`;
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-store",
+    },
+  });
+}
